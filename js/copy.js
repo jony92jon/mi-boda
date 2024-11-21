1e3d837318ac902931bc56b0ca529ec901decd9e
ismael bakkali <ismaelbakkalichairi@gmail.com>
2024-11-21 13:11:12 +0000

    invitation

--- js/copy.js
+++ js/copy.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const copyButton = document.getElementById("copyButton");
-    const accountNumber = document.getElementById("accountNumber");
-  
-    copyButton.addEventListener("click", async () => {
-      try {
-        await navigator.clipboard.writeText(accountNumber.textContent);
-        console.log("Copied to clipboard");
-      } catch (err) {
-        console.error("Failed to copy text: ", err);
-      }
-    });
+  const copyButton = document.getElementById("copyButton");
+  const accountNumber = document.getElementById("accountNumber");
+
+  copyButton.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(accountNumber.textContent);
+      console.log("Copied to clipboard");
+
+      toast.classList.remove("hidden");
+      setTimeout(() => {
+        toast.classList.remove("-translate-y-full", "opacity-0");
+      }, 100);
+
+      setTimeout(() => {
+        toast.classList.add("-translate-y-full", "opacity-0");
+        setTimeout(() => {
+          toast.classList.add("hidden");
+        }, 300);
+      }, 3000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
   });
+});
